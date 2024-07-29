f002568e15d2d36d78bf566564685a0ec62df34d
PataPom <dylan@DELL-L7480-01.hbdcloud.local>
2024-07-29 20:31:46 +0200

    Adding licence part in all classes

--- src/main/java/fr/tmmods/tmapi/exceptions/ProfileNotFoundException.java
+++ src/main/java/fr/tmmods/tmapi/exceptions/ProfileNotFoundException.java
@@ -2,6 +2,23 @@ package fr.tmmods.tmapi.exceptions;
 
 import java.util.UUID;
 
+/**
+ * This file is part of TM-API, a Spigot/BungeeCord API.
+ *
+ * TM-API is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TM-API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 @Deprecated
 public class ProfileNotFoundException extends Exception
 {
